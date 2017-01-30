8887c63f919e66369327304b399dc779c19f7f14
Dmitry <dmitryscaletta@mail.ru>
2017-01-31 01:17:16 +0300

    Add redux and react-router

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,14 +1,35 @@
-import React    from 'react'
-import * as api from '../api'
+import React, { Component } from 'react'
+import { Provider }         from 'react-redux'
+import { hashHistory, Router, Route, /*IndexRoute*/ } from 'react-router'
+import configureStore       from '../store/configureStore'
+import * as api             from '../api'
 
 // console.log(api.getPlanetsWithLifeByGalaxy(12))
 // console.table(api.getPlanetsWithMinRadiusAndMaxSatellitesCount())
 // console.table(api.getPlanetWithMaxSatellitesAndMinSatellitesVolume())
 // console.table(api.getGalaxyWithMaxSumOfCoreTemperatures())
 
-const App = () =>
+const Main = () =>
 	<div>
 		No content here. We only test the build process 😉
 	</div>
 
-export default App
+const store = configureStore()
+
+export default class App extends Component {
+	render() {
+		return (
+			<Provider store={store}>
+				<Router history={hashHistory}>
+					<Route path='/' component={Main}>
+						{/*<IndexRoute component={Home} />*/}
+					</Route>
+				</Router>
+			</Provider>
+		)
+	}
+}
+
+// App.propTypes = {
+	// store: React.PropTypes.object.isRequired,
+// }
---
+++ src/store/configureStore.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk       from 'redux-thunk'
+import rootReducer from '../reducers'
+
+export default function configureStore(preloadedState) {
+	if (process.env.NODE_ENV === 'production') {
+		return createStore(
+			rootReducer, 
+			preloadedState,
+			applyMiddleware(thunk)
+		)
+	} else {
+		return createStore(
+			rootReducer, 
+			preloadedState,
+			compose(
+				applyMiddleware(thunk),
+				window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+			)
+		)
+	}
+}
